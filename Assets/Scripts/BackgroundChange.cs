adb1299c584af9a3d50a2bf4ea5f9d4223de85b7
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-07-28 19:16:21 -0600

    testing dialogue system

--- Assets/Scripts/BackgroundChange.cs
+++ Assets/Scripts/BackgroundChange.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class BackgroundChange : MonoBehaviour
 {
-    public GameObject skyBackground;
-    public GameObject treesBackground;
-    public GameObject cavern;
-    public PlayerMovement player;
+    public CanvasGroup skyBackground;
+    public CanvasGroup treesBackground;
+    public CanvasGroup cavern;
+    private PlayerMovement player;
+
+    Coroutine currentActiveFade = null;
+
 
     private void Awake()
     {
@@ -23,15 +26,43 @@ public class BackgroundChange : MonoBehaviour
     {
         if (player.transform.position.y < -10)
         {
-            cavern.SetActive(true);
-            skyBackground.SetActive(false);
-            treesBackground.SetActive(false);
+            FadeIn(cavern, 2f);
+            FadeOut(treesBackground, 2f);
+            FadeOut(skyBackground, 2f);
         } else
         {
-            cavern.SetActive(false);
-            skyBackground.SetActive(true);
-            treesBackground.SetActive(true);
+            FadeOut(cavern, 2f);
+            FadeIn(treesBackground, 2f);
+            FadeIn(skyBackground, 2f);
         }
+    }
 
+    public Coroutine FadeOut(CanvasGroup canvasGroup,  float time)
+    {
+        return Fade(canvasGroup, 0, time);
+    }
+
+    public Coroutine FadeIn(CanvasGroup canvasGroup, float time)
+    {
+        return Fade(canvasGroup, 1, time);
+    }
+
+    public Coroutine Fade(CanvasGroup canvasGroup, float target, float time)
+    {
+        if (currentActiveFade != null)
+        {
+            StopCoroutine(currentActiveFade);
+        }
+        currentActiveFade = StartCoroutine(FadeRoutine(canvasGroup, target, time));
+        return currentActiveFade;
+    }
+
+    private IEnumerator FadeRoutine(CanvasGroup canvasGroup, float target, float time)
+    {
+        while (!Mathf.Approximately(canvasGroup.alpha, target))
+        {
+            canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, target, Time.deltaTime / time);
+            yield return null;
+        }
     }
 }
