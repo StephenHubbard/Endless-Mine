cae1cfecf0c0bc9b90b1adba8acd1dad39af990c
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-07-30 13:10:39 -0600

    ui makeover

---
+++ Assets/Scripts/Inventory/BackpackInventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BackpackInventory : MonoBehaviour
+{
+    public TextMeshProUGUI amountInSlot1;
+    public int amountOfDirt = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        updateAmountInSlot();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateAmountInSlot()
+    {
+        amountInSlot1.text = amountOfDirt.ToString();
+    }
+}
---
+++ Assets/Scripts/Inventory/Pickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    BackpackInventory backpackInventory;
+    private bool pickedUp = false;
+
+    private void Awake()
+    {
+        backpackInventory = FindObjectOfType<BackpackInventory>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player" && pickedUp == false)
+        {
+            pickedUp = true;
+
+            if (gameObject.GetComponent<Dirt>())
+            {
+                backpackInventory.amountOfDirt += 1;
+                backpackInventory.updateAmountInSlot();
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
