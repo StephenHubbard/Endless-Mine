5bc05ce6c388b2b2b7bd9c63a67ee739fe11994b
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-08-03 12:01:18 -0600

    day clock and corresponding lighting

---
+++ Assets/Scripts/UI/DayLight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class DayLight : MonoBehaviour
+{
+    private Light2D lightSource;
+    private float timeOfDayFloat;
+    private float lightSourceIntensity = .85f;
+    private float lightSourceIntensityClamped;
+
+    void Awake()
+    {
+        lightSource = GetComponent<Light2D>();
+    }
+
+    void Update()
+    {
+        timeOfDayFloat = FindObjectOfType<TimeOfDay>().day;
+        if (timeOfDayFloat > .666f)
+        {
+            lightSourceIntensity -= Time.deltaTime / 4.8f;
+            lightSourceIntensityClamped = Mathf.Clamp(lightSourceIntensity, 0f, 1f);
+            lightSource.intensity = lightSourceIntensityClamped;
+
+            if (lightSourceIntensityClamped < .85f)
+            {
+                // hacky way to fix light layering bug with torches on top level
+                gameObject.SetActive(false);
+                gameObject.SetActive(true);
+            }
+        }
+
+    }
+}
--- Assets/Scripts/UI/TimeOfDay.cs
+++ Assets/Scripts/UI/TimeOfDay.cs
@@ -6,9 +6,31 @@ using TMPro;
 
 public class TimeOfDay : MonoBehaviour
 {
-    
-    void Update()
+    public TextMeshProUGUI timeOfDayText;
+
+    //private const float REAL_SECONDS_PER_INGAME_DAY = 1440f;
+    private const float REAL_SECONDS_PER_INGAME_DAY = 14.4f;
+
+    public float day;
+
+    private float hoursPerDay = 24f;
+    private float minutesPerHour = 60f;
+
+    private void Awake()
     {
-        
+        day += 0.33333f;
+    }
+
+    private void Update()
+    {
+        day += Time.deltaTime / REAL_SECONDS_PER_INGAME_DAY;
+
+        float dayNormalized = day % 1f;
+
+        string hoursString = Mathf.Floor(dayNormalized * hoursPerDay).ToString("00");
+        string minutesString = Mathf.Floor(((dayNormalized * hoursPerDay) % 1f) * minutesPerHour).ToString("00");
+
+        timeOfDayText.text = hoursString + ":" + minutesString + " AM";
     }
 }
+
